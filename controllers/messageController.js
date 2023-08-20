75f4fb1932d738dea66ba99a584a19d40baa6be5
Rajiv Shrestha <rajivnewar123@gmail.com>
2023-08-20 17:53:21 +0545

    bug fixed

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -43,6 +43,18 @@ class MessageController {
     console.log(req.body);
     res.status(200).json({ message: "hello", success: true });
   }
+  async deleteMessage(req, res) {
+    const { msgId: _id } = req.params;
+
+    try {
+      await MessageModel.findOneAndDelete({
+        _id,
+      });
+      res.status(200).json({ message: "successfully deleted", success: true });
+    } catch (error) {
+      res.status(500).json({ message: error.message, success: false });
+    }
+  }
 }
 
 module.exports = new MessageController();
--- routes/messageRoute.js
+++ routes/messageRoute.js
@@ -2,6 +2,7 @@ const { createChatForFirstMessage } = require("../controllers/ChatController");
 const {
   addMessage,
   getMessageOfAChat,
+  deleteMessage,
 } = require("../controllers/MessageController");
 
 const router = require("express").Router();
@@ -9,5 +10,6 @@ const router = require("express").Router();
 router.post("/create", addMessage);
 router.post("/new_message", createChatForFirstMessage, addMessage);
 router.get("/:chatId", getMessageOfAChat);
+router.delete("/:msgId", deleteMessage);
 
 module.exports = router;
